59e3eece8e93e9973611ac04f7e534305cdb3340
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-11-12 15:21:58 -0300

    cálculo de média

---
+++ mod-02/aula-js/calculo-media.js
@@ -0,0 +1,12 @@
+// criar um programa que calcule a média das notas entre os alunos e envie mensagem do cálculo da média
+
+const aluno1 = "Angélica";
+const aluno2 = "Flávio";
+const aluno3 = "Vitor";
+const notaAluno1 = 9.8;
+const notaAluno2 = 10;
+const notaAluno3 = 2;
+
+const media = (notaAluno1 + notaAluno2 + notaAluno3) / 3
+
+console.log(media)
