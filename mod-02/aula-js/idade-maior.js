252296ea3cef0c36e65e3dd9dd6d2ec5e1d4cca4
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-11-12 20:38:28 -0300

    Desafio maior de idade

---
+++ mod-02/aula-js/idade-maior.js
@@ -0,0 +1,17 @@
+// DESAFIO 1
+
+// Verificar se a pessoa é maior igual de 18 anos. 
+// Se sim, deixar entrar; se não, bloquear a entrada.
+// Se a pessoa tiver 17 anos, avisar para voltar quando fizer 18.
+
+const idade = 17
+
+if (idade >= 18) {
+ console.log("Você pode entrar.");
+} else {
+  console.log("Você não pode entrar.")
+}
+
+if (idade === 17) {
+  console.log("Volte quando fizer 18 anos.")
+}
